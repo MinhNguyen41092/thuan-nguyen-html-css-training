8d03cb332f8cfcbcae1e0dcf9f454cc7de5e05a2
thuannguyenhuu11 <thuannguyenhuu11@gmail.com>
2023-08-02 10:35:01 +0700

    Fix contact-section

--- practice-two/src/js/dropdown.js
+++ practice-two/src/js/dropdown.js
@@ -10,3 +10,27 @@ icon.addEventListener('click', function () {
 navbarBtn.addEventListener('click', function () {
   navbar.classList.remove('active');
 });
+
+
+// const questionOpen = document.querySelector('.question__btn--open');
+// const questionClose = document.querySelector('.question__btn--close');
+// const question = document.querySelector('.question');
+// const answer = document.querySelector('.answer');
+
+// console.log(questionOpen)
+
+// questionOpen.addEventListener('click', function () {
+//   questionOpen.classList.toggle('q-un-active');
+//   questionClose.classList.toggle('q-active');
+//   question.classList.toggle('text--primary');
+//   question.classList.toggle('text--bold');
+//   answer.classList.toggle('a-active')
+// });
+
+// questionClose.addEventListener('click', function () {
+//   questionOpen.classList.remove('q-un-active');
+//   questionClose.classList.remove('q-active');
+//   question.classList.remove('text--primary');
+//   question.classList.remove('text--bold');
+//   answer.classList.remove('a-active')
+// });
